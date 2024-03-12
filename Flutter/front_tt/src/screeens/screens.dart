f3238afc525c3887d2c2587252d0a89ce6f95f5b
Pepe-AI <jriosp1900@alumno.ipn.mx>
2024-03-11 21:35:25 -0600

    Eda finalizado

--- Flutter/front_tt/src/screeens/screens.dart
+++
@@ -1,4 +0,0 @@
-export 'card_screen.dart';
-export 'home_screen.dart';
-export 'alert_screen.dart';
-export 'animated_screen.dart';
--- Flutter/front_tt/src/widgets/widgets.dart
+++
@@ -1,3 +0,0 @@
-export 'custom_card_type_1.dart';
-export 'custom_card_type_2.dart';
-export 'custom_input_field.dart';
